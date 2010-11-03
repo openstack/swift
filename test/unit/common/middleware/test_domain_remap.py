67504c595a62cb8ab3e5bd0399f0d4521fb5d283
gholt <gholt@rackspace.com>
2010-11-03 09:04:44 -0700

    PEPy fixes

--- test/unit/common/middleware/test_domain_remap.py
+++ test/unit/common/middleware/test_domain_remap.py
@@ -19,12 +19,16 @@ from webob import Request
 
 from swift.common.middleware import domain_remap
 
+
 class FakeApp(object):
+
     def __call__(self, env, start_response):
         return env['PATH_INFO']
 
+
 def start_response(*args):
     pass
+
 
 class TestDomainRemap(unittest.TestCase):
 
