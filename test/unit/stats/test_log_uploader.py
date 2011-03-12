211f4fa5e1ba2a56eba425549b27feadf60ea70b
Clay Gerrard <clay.gerrard@rackspace.com>
2011-03-11 18:07:17 -0600

    removed old comment

--- test/unit/stats/test_log_uploader.py
+++ test/unit/stats/test_log_uploader.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: More tests
-
 import unittest
 import os
 from datetime import datetime
