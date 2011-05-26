2d1274c4974c7231a00cd835aedb31b7a349e710
Soren Hansen <soren@linux2go.dk>
2011-05-26 11:25:39 +0200

    Add a __canonical_version__ attribute to the swift module. This is only the numbered part of the version string, no "-dev" or similar suffixes.
    Add a couple of unit tests to make sure __version__ and __canonical_version__ are generated correctly and to ensure __canonical_version__ never accidentally has anything other than numbers and points in it.

--- swift/__init__.py
+++ swift/__init__.py
@@ -1,5 +1,21 @@
 import gettext
 
 
-__version__ = '1.4-dev'
+class Version(object):
+    def __init__(self, canonical_version, final):
+        self.canonical_version = canonical_version
+        self.final = final
+
+    @property
+    def pretty_version(self):
+        if self.final:
+           return self.canonical_version
+        else:
+           return '%s-dev' % (self.canonical_version,)
+
+
+_version = Version('1.4.1', False)
+__version__ = _version.pretty_version
+__canonical_version__ = _version.canonical_version
+
 gettext.install('swift')
