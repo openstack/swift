1fd82fde79a6533bba18eae1a22a88cb95a8cec6
David Goetz <david.goetz@rackspace.com>
2011-08-17 11:29:21 -0700

    fix to wsgi request

--- swift/common/wsgi.py
+++ swift/common/wsgi.py
@@ -26,6 +26,7 @@ import eventlet
 from eventlet import greenio, GreenPool, sleep, wsgi, listen
 from paste.deploy import loadapp, appconfig
 from eventlet.green import socket, ssl
+from webob import Request
 
 from swift.common.utils import get_logger, drop_privileges, \
     validate_configuration, capture_stdio, NullLogger
@@ -190,7 +191,7 @@ def run_wsgi(conf_file, app_section, *args, **kwargs):
     logger.notice('Exited')
 
 
-def make_pre_authed_request(self, env, method, path, body=None, headers=None,
+def make_pre_authed_request(env, method, path, body=None, headers=None,
                             agent='Swift'):
     """
     Makes a new webob.Request based on the current env but with the
