012c94a681cad6f397f272517462cf92015cd7b6
Thierry Carrez <thierry@openstack.org>
2012-05-30 16:28:46 +0200

    Final versioning for 1.5.0
    Final versioning for 1.5.0. This should be merged as the last
    commit before 1.5.0 final release !
    
    Change-Id: I6663036993fb0a5cf12fc7fa3f4e1bc261b79ef4

--- swift/__init__.py
+++ swift/__init__.py
@@ -14,7 +14,7 @@ class Version(object):
             return '%s-dev' % (self.canonical_version,)
 
 
-_version = Version('1.5.0', False)
+_version = Version('1.5.0', True)
 __version__ = _version.pretty_version
 __canonical_version__ = _version.canonical_version
 
