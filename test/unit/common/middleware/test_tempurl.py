8d681f0618b05233bc74336ac48870ca2d6bfce3
gholt <z-launchpad@brim.net>
2013-05-29 15:02:06 +0000

    Subtle change to tempurl content-disposition names
    If an object name ends with a /, the content-disposition filename for
    a tempurl ended up as an empty string. This fix just strips any
    trailing slashes before calling basename.
    
    Also, I added a test to verify that if the filename is passed as an
    overriding query parameter, that it is used in full, no rstripping,
    no basenaming.
    
    Change-Id: I37725b6ded04ed3b91cdb21132490fd857276e2f

--- test/unit/common/middleware/test_tempurl.py
+++ test/unit/common/middleware/test_tempurl.py
@@ -153,6 +153,44 @@ class TestTempURL(unittest.TestCase):
         self.assertEquals(req.environ['swift.authorize_override'], True)
         self.assertEquals(req.environ['REMOTE_USER'], '.wsgi.tempurl')
 
+    def test_obj_trailing_slash(self):
+        method = 'GET'
+        expires = int(time() + 86400)
+        path = '/v1/a/c/o/'
+        key = 'abc'
+        hmac_body = '%s\n%s\n%s' % (method, expires, path)
+        sig = hmac.new(key, hmac_body, sha1).hexdigest()
+        req = self._make_request(path, environ={
+            'QUERY_STRING': 'temp_url_sig=%s&temp_url_expires=%s' % (
+                sig, expires)})
+        req.environ['swift.cache'].set('temp-url-keys/a', [key])
+        self.tempurl.app = FakeApp(iter([('200 Ok', (), '123')]))
+        resp = req.get_response(self.tempurl)
+        self.assertEquals(resp.status_int, 200)
+        self.assertEquals(resp.headers['content-disposition'],
+                          'attachment; filename="o"')
+        self.assertEquals(req.environ['swift.authorize_override'], True)
+        self.assertEquals(req.environ['REMOTE_USER'], '.wsgi.tempurl')
+
+    def test_filename_trailing_slash(self):
+        method = 'GET'
+        expires = int(time() + 86400)
+        path = '/v1/a/c/o'
+        key = 'abc'
+        hmac_body = '%s\n%s\n%s' % (method, expires, path)
+        sig = hmac.new(key, hmac_body, sha1).hexdigest()
+        req = self._make_request(path, environ={
+            'QUERY_STRING': 'temp_url_sig=%s&temp_url_expires=%s&'
+            'filename=/i/want/this/just/as/it/is/' % (sig, expires)})
+        req.environ['swift.cache'].set('temp-url-keys/a', [key])
+        self.tempurl.app = FakeApp(iter([('200 Ok', (), '123')]))
+        resp = req.get_response(self.tempurl)
+        self.assertEquals(resp.status_int, 200)
+        self.assertEquals(resp.headers['content-disposition'],
+                          'attachment; filename="/i/want/this/just/as/it/is/"')
+        self.assertEquals(req.environ['swift.authorize_override'], True)
+        self.assertEquals(req.environ['REMOTE_USER'], '.wsgi.tempurl')
+
     def test_get_valid_but_404(self):
         method = 'GET'
         expires = int(time() + 86400)
