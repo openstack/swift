1f43ee050b94d368182d1d6e0340416ea1436e76
Clay Gerrard <clay.gerrard@gmail.com>
2013-07-19 01:39:42 -0700

    Add option to make probetests more brittle
    Currently probetests take advantage of a number of assumptions about the SUT.
    Unfortunately after some time with a working SAIO, configuration drift may
    result in a system that is no longer compatible with these assumptions.  To
    help weary developers more quickly identify the changes they've made since
    they last ran probetests successfully, some handy validators have been added
    to test.probe.common
    
    Additionally a new option 'validate_rsync' in test.conf, when enabled, will
    run a series of up front validations during the setup of each probetest by
    inspecting the ring, the mounted devices, and the rsync exports ("modules") in
    order to ensure that when probetests fail the do so early and with specific
    complaints.
    
    To preserve existing failures, the option is disabled by default.
    
    Change-Id: I2be11c7e67ccd0bc0589c360c170049b6288c152

--- test/probe/__init__.py
+++ test/probe/__init__.py
@@ -1,3 +1,5 @@
 from test import get_config
+from swift.common.utils import config_true_value
 config = get_config('probe_test')
 CHECK_SERVER_TIMEOUT = int(config.get('check_server_timeout', 30))
+VALIDATE_RSYNC = config_true_value(config.get('validate_rsync', False))
