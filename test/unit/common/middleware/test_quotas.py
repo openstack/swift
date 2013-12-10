946984cb84414c84fe38d4b94b73174960e023b8
Fabien Boucher <fabien.boucher@enovance.com>
2013-12-10 18:31:55 +0100

    Handle COPY verb in container quota middleware
    The patch fix the ability to bypass the account
    quota using the COPY verb. The quotas for destination
    container are now checked before the copy.
    
    Change-Id: I9f409ea6c0893f36972aa1fabe31fb143e151ab5
    Fixes: bug #1259565

--- test/unit/common/middleware/test_quotas.py
+++ test/unit/common/middleware/test_quotas.py
@@ -106,6 +106,18 @@ class TestContainerQuotas(unittest.TestCase):
         res = req.get_response(app)
         self.assertEquals(res.status_int, 413)
 
+    def test_exceed_bytes_quota_copy_verb(self):
+        app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
+        cache = FakeCache({'bytes': 0, 'meta': {'quota-bytes': '2'}})
+
+        req = Request.blank('/v1/a/c2/o2',
+                            environ={'REQUEST_METHOD': 'COPY',
+                            'swift.object/a/c2/o2': {'length': 10},
+                            'swift.cache': cache},
+                            headers={'Destination': '/c/o'})
+        res = req.get_response(app)
+        self.assertEquals(res.status_int, 413)
+
     def test_not_exceed_bytes_quota(self):
         app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
         cache = FakeCache({'bytes': 0, 'meta': {'quota-bytes': '100'}})
@@ -124,6 +136,17 @@ class TestContainerQuotas(unittest.TestCase):
                             'swift.object/a/c2/o2': {'length': 10},
                             'swift.cache': cache},
                             headers={'x-copy-from': '/c2/o2'})
+        res = req.get_response(app)
+        self.assertEquals(res.status_int, 200)
+
+    def test_not_exceed_bytes_quota_copy_verb(self):
+        app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
+        cache = FakeCache({'bytes': 0, 'meta': {'quota-bytes': '100'}})
+        req = Request.blank('/v1/a/c2/o2',
+                            environ={'REQUEST_METHOD': 'COPY',
+                            'swift.object/a/c2/o2': {'length': 10},
+                            'swift.cache': cache},
+                            headers={'Destination': '/c/o'})
         res = req.get_response(app)
         self.assertEquals(res.status_int, 200)
 
@@ -148,6 +171,17 @@ class TestContainerQuotas(unittest.TestCase):
         res = req.get_response(app)
         self.assertEquals(res.status_int, 412)
 
+    def test_bytes_quota_copy_verb_no_src(self):
+        app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
+        cache = FakeCache({'bytes': 0, 'meta': {'quota-bytes': '100'}})
+        req = Request.blank('/v1/a/c2/o3',
+                            environ={'REQUEST_METHOD': 'COPY',
+                            'swift.object/a/c2/o2': {'length': 10},
+                            'swift.cache': cache},
+                            headers={'Destination': '/c/o'})
+        res = req.get_response(app)
+        self.assertEquals(res.status_int, 200)
+
     def test_exceed_counts_quota(self):
         app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
         cache = FakeCache({'object_count': 1, 'meta': {'quota-count': '1'}})
@@ -169,6 +203,16 @@ class TestContainerQuotas(unittest.TestCase):
         res = req.get_response(app)
         self.assertEquals(res.status_int, 413)
 
+    def test_exceed_counts_quota_copy_verb(self):
+        app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
+        cache = FakeCache({'object_count': 1, 'meta': {'quota-count': '1'}})
+        req = Request.blank('/v1/a/c2/o2',
+                            environ={'REQUEST_METHOD': 'COPY',
+                            'swift.cache': cache},
+                            headers={'Destination': '/c/o'})
+        res = req.get_response(app)
+        self.assertEquals(res.status_int, 413)
+
     def test_not_exceed_counts_quota(self):
         app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
         cache = FakeCache({'object_count': 1, 'meta': {'quota-count': '2'}})
@@ -186,6 +230,16 @@ class TestContainerQuotas(unittest.TestCase):
                             environ={'REQUEST_METHOD': 'PUT',
                             'swift.cache': cache},
                             headers={'x-copy-from': '/c2/o2'})
+        res = req.get_response(app)
+        self.assertEquals(res.status_int, 200)
+
+    def test_not_exceed_counts_quota_copy_verb(self):
+        app = container_quotas.ContainerQuotaMiddleware(FakeApp(), {})
+        cache = FakeCache({'object_count': 1, 'meta': {'quota-count': '2'}})
+        req = Request.blank('/v1/a/c2/o2',
+                            environ={'REQUEST_METHOD': 'COPY',
+                            'swift.cache': cache},
+                            headers={'Destination': '/c/o'})
         res = req.get_response(app)
         self.assertEquals(res.status_int, 200)
 
