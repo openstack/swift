0b893825ebdd63d22d51cce42a3994f120f43921
Chuck Thier <cthier@gmail.com>
2014-03-19 21:51:17 +0000

    Add "If-None-Match: *" support to PUT
    A common pattern that we see clients do is send a HEAD request before a
    PUT to see if it exists.  This can slow things down quite a bit
    especially since 404s on HEAD are currently a bit expensive.
    
    This change will allow a client to include a "If-None-Match: *" header
    with a PUT request.  In combination with "Expect: 100-Continue" this
    allows the server to return that it already has a copy of the object
    before any data is sent.
    
    I attempted to also include etag support with the If-None-Match header,
    but that turned up having too many hairy edge cases, so was left as a
    future excercise.
    
    DocImpact
    
    Change-Id: I94e3754923dbe5faba065719c7a9afa9969652dd

--- test/unit/proxy/controllers/test_obj.py
+++ test/unit/proxy/controllers/test_obj.py
@@ -22,7 +22,7 @@ import mock
 import swift
 from swift.proxy import server as proxy_server
 from swift.common.swob import HTTPException
-from test.unit import FakeRing, FakeMemcache, fake_http_connect
+from test.unit import FakeRing, FakeMemcache, fake_http_connect, debug_logger
 
 
 @contextmanager
@@ -90,26 +90,96 @@ class TestObjControllerWriteAffinity(unittest.TestCase):
 
 
 class TestObjController(unittest.TestCase):
+    def setUp(self):
+        logger = debug_logger('proxy-server')
+        logger.thread_locals = ('txn1', '127.0.0.2')
+        self.app = proxy_server.Application(
+            None, FakeMemcache(), account_ring=FakeRing(),
+            container_ring=FakeRing(), object_ring=FakeRing(),
+            logger=logger)
+        self.controller = proxy_server.ObjectController(self.app,
+                                                        'a', 'c', 'o')
+        self.controller.container_info = mock.MagicMock(return_value={
+            'partition': 1,
+            'nodes': [
+                {'ip': '127.0.0.1', 'port': '1', 'device': 'sda'},
+                {'ip': '127.0.0.1', 'port': '2', 'device': 'sda'},
+                {'ip': '127.0.0.1', 'port': '3', 'device': 'sda'},
+            ],
+            'write_acl': None,
+            'read_acl': None,
+            'sync_key': None,
+            'versions': None})
+
+    def test_PUT_simple(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        req.headers['content-length'] = '0'
+        with set_http_connect(201, 201, 201):
+            resp = self.controller.PUT(req)
+        self.assertEquals(resp.status_int, 201)
+
+    def test_PUT_if_none_match(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        req.headers['if-none-match'] = '*'
+        req.headers['content-length'] = '0'
+        with set_http_connect(201, 201, 201):
+            resp = self.controller.PUT(req)
+        self.assertEquals(resp.status_int, 201)
+
+    def test_PUT_if_none_match_denied(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        req.headers['if-none-match'] = '*'
+        req.headers['content-length'] = '0'
+        with set_http_connect(201, (412, 412), 201):
+            resp = self.controller.PUT(req)
+        self.assertEquals(resp.status_int, 412)
+
+    def test_PUT_if_none_match_not_star(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        req.headers['if-none-match'] = 'somethingelse'
+        req.headers['content-length'] = '0'
+        with set_http_connect(201, 201, 201):
+            resp = self.controller.PUT(req)
+        self.assertEquals(resp.status_int, 400)
+
+    def test_GET_simple(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        with set_http_connect(200):
+            resp = self.controller.GET(req)
+        self.assertEquals(resp.status_int, 200)
+
+    def test_DELETE_simple(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        with set_http_connect(204, 204, 204):
+            resp = self.controller.DELETE(req)
+        self.assertEquals(resp.status_int, 204)
+
+    def test_POST_simple(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        with set_http_connect(200, 200, 200, 201, 201, 201):
+            resp = self.controller.POST(req)
+        self.assertEquals(resp.status_int, 202)
+
+    def test_COPY_simple(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        with set_http_connect(200, 200, 200, 201, 201, 201):
+            resp = self.controller.POST(req)
+        self.assertEquals(resp.status_int, 202)
+
+    def test_HEAD_simple(self):
+        req = swift.common.swob.Request.blank('/v1/a/c/o')
+        with set_http_connect(200, 200, 200, 201, 201, 201):
+            resp = self.controller.POST(req)
+        self.assertEquals(resp.status_int, 202)
 
     def test_PUT_log_info(self):
         # mock out enough to get to the area of the code we want to test
         with mock.patch('swift.proxy.controllers.obj.check_object_creation',
                         mock.MagicMock(return_value=None)):
-            app = mock.MagicMock()
-            app.container_ring.get_nodes.return_value = (1, [2])
-            app.object_ring.get_nodes.return_value = (1, [2])
-            controller = proxy_server.ObjectController(app, 'a', 'c', 'o')
-            controller.container_info = mock.MagicMock(return_value={
-                'partition': 1,
-                'nodes': [{}],
-                'write_acl': None,
-                'sync_key': None,
-                'versions': None})
-            # and now test that we add the header to log_info
             req = swift.common.swob.Request.blank('/v1/a/c/o')
             req.headers['x-copy-from'] = 'somewhere'
             try:
-                controller.PUT(req)
+                self.controller.PUT(req)
             except HTTPException:
                 pass
             self.assertEquals(
@@ -119,7 +189,7 @@ class TestObjController(unittest.TestCase):
             req.method = 'POST'
             req.headers['x-copy-from'] = 'elsewhere'
             try:
-                controller.PUT(req)
+                self.controller.PUT(req)
             except HTTPException:
                 pass
             self.assertEquals(req.environ.get('swift.log_info'), None)
