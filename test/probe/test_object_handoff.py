a4ffd1d1c6f8027d408c73076113ddafdc79ebc0
Leah Klearman <lklrmn@gmail.com>
2015-02-12 20:32:35 -0800

    move test comments around
    should make later refactoring easier
    
    Change-Id: I7af399a14c8bc78fcfc438e4440d2f023c8aa5db

--- test/probe/test_object_handoff.py
+++ test/probe/test_object_handoff.py
@@ -29,33 +29,10 @@ class TestObjectHandoff(ReplProbeTest):
 
     def test_main(self):
         # Create container
-        # Kill one container/obj primary server
-        # Create container/obj (goes to two primary servers and one handoff)
-        # Kill other two container/obj primary servers
-        # Indirectly through proxy assert we can get container/obj
-        # Restart those other two container/obj primary servers
-        # Directly to handoff server assert we can get container/obj
-        # Assert container listing (via proxy and directly) has container/obj
-        # Bring the first container/obj primary server back up
-        # Assert that it doesn't have container/obj yet
-        # Run object replication, ensuring we run the handoff node last so it
-        #   should remove its extra handoff partition
-        # Assert the first container/obj primary server now has container/obj
-        # Assert the handoff server no longer has container/obj
-        # Kill the first container/obj primary server again (we have two
-        #   primaries and the handoff up now)
-        # Delete container/obj
-        # Assert we can't head container/obj
-        # Assert container/obj is not in the container listing, both indirectly
-        #   and directly
-        # Restart the first container/obj primary server again
-        # Assert it still has container/obj
-        # Run object replication, ensuring we run the handoff node last so it
-        #   should remove its extra handoff partition
-        # Assert primary node no longer has container/obj
         container = 'container-%s' % uuid4()
         client.put_container(self.url, self.token, container)
 
+        # Kill one container/obj primary server
         cpart, cnodes = self.container_ring.get_nodes(self.account, container)
         cnode = cnodes[0]
         obj = 'object-%s' % uuid4()
@@ -63,22 +40,31 @@ class TestObjectHandoff(ReplProbeTest):
             self.account, container, obj)
         onode = onodes[0]
         kill_server(onode['port'], self.port2server, self.pids)
+
+        # Create container/obj (goes to two primary servers and one handoff)
         client.put_object(self.url, self.token, container, obj, 'VERIFY')
         odata = client.get_object(self.url, self.token, container, obj)[-1]
         if odata != 'VERIFY':
             raise Exception('Object GET did not return VERIFY, instead it '
                             'returned: %s' % repr(odata))
-        # Kill all primaries to ensure GET handoff works
+
+        # Kill other two container/obj primary servers
+        #   to ensure GET handoff works
         for node in onodes[1:]:
             kill_server(node['port'], self.port2server, self.pids)
+
+        # Indirectly through proxy assert we can get container/obj
         odata = client.get_object(self.url, self.token, container, obj)[-1]
         if odata != 'VERIFY':
             raise Exception('Object GET did not return VERIFY, instead it '
                             'returned: %s' % repr(odata))
+
+        # Restart those other two container/obj primary servers
         for node in onodes[1:]:
             start_server(node['port'], self.port2server, self.pids)
-        # We've indirectly verified the handoff node has the object, but let's
-        # directly verify it.
+
+        # We've indirectly verified the handoff node has the container/object,
+        #   but let's directly verify it.
         another_onode = self.object_ring.get_more_nodes(opart).next()
         odata = direct_client.direct_get_object(
             another_onode, opart, self.account, container, obj, headers={
@@ -86,6 +72,8 @@ class TestObjectHandoff(ReplProbeTest):
         if odata != 'VERIFY':
             raise Exception('Direct object GET did not return VERIFY, instead '
                             'it returned: %s' % repr(odata))
+
+        # Assert container listing (via proxy and directly) has container/obj
         objs = [o['name'] for o in
                 client.get_container(self.url, self.token, container)[1]]
         if obj not in objs:
@@ -98,7 +86,11 @@ class TestObjectHandoff(ReplProbeTest):
                 raise Exception(
                     'Container server %s:%s did not know about object' %
                     (cnode['ip'], cnode['port']))
+
+        # Bring the first container/obj primary server back up
         start_server(onode['port'], self.port2server, self.pids)
+
+        # Assert that it doesn't have container/obj yet
         exc = None
         try:
             direct_client.direct_get_object(
@@ -107,7 +99,9 @@ class TestObjectHandoff(ReplProbeTest):
         except ClientException as err:
             exc = err
         self.assertEquals(exc.http_status, 404)
-        # Run the extra server last so it'll remove its extra partition
+
+        # Run object replication, ensuring we run the handoff node last so it
+        #   will remove its extra handoff partition
         for node in onodes:
             try:
                 port_num = node['replication_port']
@@ -121,12 +115,16 @@ class TestObjectHandoff(ReplProbeTest):
             another_port_num = another_onode['port']
         another_num = (another_port_num - 6000) / 10
         Manager(['object-replicator']).once(number=another_num)
+
+        # Assert the first container/obj primary server now has container/obj
         odata = direct_client.direct_get_object(
             onode, opart, self.account, container, obj, headers={
                 'X-Backend-Storage-Policy-Index': self.policy.idx})[-1]
         if odata != 'VERIFY':
             raise Exception('Direct object GET did not return VERIFY, instead '
                             'it returned: %s' % repr(odata))
+
+        # Assert the handoff server no longer has container/obj
         exc = None
         try:
             direct_client.direct_get_object(
@@ -136,7 +134,11 @@ class TestObjectHandoff(ReplProbeTest):
             exc = err
         self.assertEquals(exc.http_status, 404)
 
+        # Kill the first container/obj primary server again (we have two
+        #   primaries and the handoff up now)
         kill_server(onode['port'], self.port2server, self.pids)
+
+        # Delete container/obj
         try:
             client.delete_object(self.url, self.token, container, obj)
         except client.ClientException as err:
@@ -146,12 +148,17 @@ class TestObjectHandoff(ReplProbeTest):
             # remove this with fix for
             # https://bugs.launchpad.net/swift/+bug/1318375
             self.assertEqual(503, err.http_status)
+
+        # Assert we can't head container/obj
         exc = None
         try:
             client.head_object(self.url, self.token, container, obj)
         except client.ClientException as err:
             exc = err
         self.assertEquals(exc.http_status, 404)
+
+        # Assert container/obj is not in the container listing, both indirectly
+        #   and directly
         objs = [o['name'] for o in
                 client.get_container(self.url, self.token, container)[1]]
         if obj in objs:
@@ -164,11 +171,17 @@ class TestObjectHandoff(ReplProbeTest):
                 raise Exception(
                     'Container server %s:%s still knew about object' %
                     (cnode['ip'], cnode['port']))
+
+        # Restart the first container/obj primary server again
         start_server(onode['port'], self.port2server, self.pids)
+
+        # Assert it still has container/obj
         direct_client.direct_get_object(
             onode, opart, self.account, container, obj, headers={
                 'X-Backend-Storage-Policy-Index': self.policy.idx})
-        # Run the extra server last so it'll remove its extra partition
+
+        # Run object replication, ensuring we run the handoff node last so it
+        #   will remove its extra handoff partition
         for node in onodes:
             try:
                 port_num = node['replication_port']
@@ -178,6 +191,8 @@ class TestObjectHandoff(ReplProbeTest):
             Manager(['object-replicator']).once(number=node_id)
         another_node_id = (another_port_num - 6000) / 10
         Manager(['object-replicator']).once(number=another_node_id)
+
+        # Assert primary node no longer has container/obj
         exc = None
         try:
             direct_client.direct_get_object(
