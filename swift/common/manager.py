26bff69cb5c5f86815ea8b4592a1a0119475023b
Andreas Jaeger <aj@suse.de>
2015-04-15 09:43:26 +0200

    Fix untranslatable string
    The word "number" is not marked for translation, we cannot just add
    it this way to a string.
    Instead create two separate strings.
    
    Change-Id: I0396f1d7405987ed1c57f0344e1c5ebeca30a2e3
    Closes-Bug: #1444327

--- swift/common/manager.py
+++ swift/common/manager.py
@@ -435,8 +435,11 @@ class Server(object):
         if not conf_files:
             # maybe there's a config file(s) out there, but I couldn't find it!
             if not kwargs.get('quiet'):
-                print _('Unable to locate config %sfor %s') % (
-                    ('number %s ' % number if number else ''), self.server)
+                if number:
+                    print _('Unable to locate config number %s for %s' % (
+                        number, self.server))
+                else:
+                    print _('Unable to locate config for %s' % (self.server))
             if kwargs.get('verbose') and not kwargs.get('quiet'):
                 if found_conf_files:
                     print _('Found configs:')
