0a1f631225c5872b6dbd4574329ed19da811362f
Janie Richling <jrichli@us.ibm.com>
2015-06-20 13:52:05 -0500

    Cryptography module to be used by middleware
    See encryption spec in_progress/at_rest_encryption.html
    
    Includes a new class that handles creating and using a context
    for encryption and decryption using AES in CTR mode. Using
    CTR mode supports decrypting the response for ranged GET requests.
    
    Co-Authored-By: Christian Cachin <cca@zurich.ibm.com>
    
    Change-Id: I2eab1cd5aa46fa28123884cd057ac4c0c022e40b

---
+++ swift/common/middleware/crypto.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2015 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import binascii
+import os
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+from swift.common.utils import get_logger
+
+
+class Crypto(object):
+    """
+    Used by middleware: Calls crypto alg
+    """
+    def __init__(self, conf):
+        # TODO - remove conf if there will be no conf items
+        self.conf = conf
+        self.logger = get_logger(conf, log_route="crypto")
+
+    def _check_key(self, key):
+        if len(key) != 32:
+            raise ValueError("Invalid key: length should be 32")
+
+    def create_encryption_ctxt(self, key, iv):
+        """
+        Creates a crypto context for encrypting
+
+        :param key: 256-bit key
+        :param iv: 128-bit iv or nonce used for encryption
+        :raises: ValueError on invalid key or iv
+        :returns: an instance of :class:`CryptoContext`
+        """
+        self._check_key(key)
+        engine = Cipher(algorithms.AES(key), modes.CTR(iv),
+                        backend=default_backend())
+        enc = engine.encryptor()
+        return CryptoContext(enc, iv, 0)
+
+    def create_decryption_ctxt(self, key, iv, offset):
+        """
+        Creates a crypto context for decrypting
+
+        :param key: 256-bit key
+        :param iv: 128-bit iv or nonce used for decryption
+        :param offset: offset into the message; used for range reads
+        :raises: ValueError on invalid key, iv or offset
+        :returns: an instance of :class:`CryptoContext`
+        """
+        self._check_key(key)
+        if offset < 0:
+            raise ValueError('Invalid offset: should be >= 0')
+        if offset > 0:
+            # Adjust IV so that it is correct for decryption at offset.
+            # ( 1<< (16 *8)) is to make 'ivl' big enough so that the following
+            # bytearray.fromhex() can be successful in all conditions.
+            ivl = long(binascii.hexlify(iv), 16)
+            ivl += int(offset / 16) + (1 << (16 * 8))
+            ivstr = format(ivl, 'x')
+            iv = str(bytearray.fromhex(ivstr[(len(ivstr) - 2 * 16):]))
+
+        engine = Cipher(algorithms.AES(key), modes.CTR(iv),
+                        backend=default_backend())
+        dec = engine.decryptor()
+        # Adjust decryption boundary to AES block size of 16 bytes
+        dec.update('*' * (offset % 16))
+        return CryptoContext(dec, iv, offset)
+
+    def create_iv(self):
+        return os.urandom(16)
+
+    def get_cipher(self):
+        return 'AES_CTR_256'
+
+
+class CryptoContext(object):
+    """
+    Crypto context used in encryption middleware.  Created by calling
+    :func:`create_encryption_ctxt` or :func:`create_decryption_ctxt`.
+    """
+    def __init__(self, engine, iv, offset):
+        self.engine = engine
+        self.iv = iv
+        self.offset = offset
+
+    def update(self, chunk):
+        return self.engine.update(chunk)
+
+    def get_iv(self):
+        return self.iv
---
+++ test/unit/common/middleware/test_crypto.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2015 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+import os
+
+from swift.common.middleware.crypto import Crypto
+
+
+class TestCrypto(unittest.TestCase):
+
+    def setUp(self):
+        self.crypto = Crypto({})
+
+    def test_basic_crypto(self):
+        value = 'encrypt me'
+        key = 'objL7wjV6L79Sfs4y7dy41273l0k6Wki'
+        iv = self.crypto.create_iv()
+        enc_ctxt = self.crypto.create_encryption_ctxt(key, iv)
+        enc_val = enc_ctxt.update(value)
+        self.assertTrue(enc_val != value)
+        self.assertEqual(len(value), len(enc_val))
+        self.assertEqual(iv, enc_ctxt.get_iv())
+
+        dec_ctxt = self.crypto.create_decryption_ctxt(key, iv, 0)
+        dec_val = dec_ctxt.update(enc_val)
+        self.assertEqual(value, dec_val,
+                         'Expected value {%s} but got {%s}' % (value, dec_val))
+        self.assertEqual(iv, dec_ctxt.get_iv())
+
+    def test_enc_dec_small_chunks(self):
+        self.enc_dec_chunks(['encrypt me', 'because I', 'am sensitive'])
+
+    def test_enc_dec_large_chunks(self):
+        self.enc_dec_chunks([os.urandom(65536), os.urandom(65536)])
+
+    def enc_dec_chunks(self, chunks):
+        key = 'objL7wjV6L79Sfs4y7dy41273l0k6Wki'
+        iv = self.crypto.create_iv()
+        enc_ctxt = self.crypto.create_encryption_ctxt(key, iv)
+        enc_val = [enc_ctxt.update(chunk) for chunk in chunks]
+        self.assertTrue(''.join(enc_val) != chunks)
+        dec_ctxt = self.crypto.create_decryption_ctxt(key, iv, 0)
+        dec_val = [dec_ctxt.update(chunk) for chunk in enc_val]
+        self.assertEqual(''.join(chunks), ''.join(dec_val),
+                         'Expected value {%s} but got {%s}' %
+                         (''.join(chunks), ''.join(dec_val)))
+
+    def test_decrypt_range(self):
+        chunks = ['012345', '6789', 'abcdef']
+        key = 'objL7wjV6L79Sfs4y7dy41273l0k6Wki'
+        iv = self.crypto.create_iv()
+        enc_ctxt = self.crypto.create_encryption_ctxt(key, iv)
+        enc_val = [enc_ctxt.update(chunk) for chunk in chunks]
+        self.assertTrue(''.join(enc_val) != chunks)
+
+        # Simulate a ranged GET from byte 4 to 12 : '456789abc'
+        dec_ctxt = self.crypto.create_decryption_ctxt(key, iv, 4)
+        ranged_chunks = [enc_val[0][4:], enc_val[1], enc_val[2][:3]]
+        dec_val = [dec_ctxt.update(chunk) for chunk in ranged_chunks]
+        self.assertEqual('456789abc', ''.join(dec_val),
+                         'Expected value {%s} but got {%s}' %
+                         ('456789abc', ''.join(dec_val)))
+
+    def test_invalid_key(self):
+        iv = self.crypto.create_iv()
+        for key in ('objKey', 'a' * 31, 'a' * 33, 'a' * 16, 'a' * 24):
+            try:
+                self.crypto.create_encryption_ctxt(key, iv)
+                self.fail('Expected ValueError to be raised for key %s' % key)
+            except ValueError as err:
+                self.assertTrue(err.message.startswith('Invalid key'))
+            try:
+                self.crypto.create_decryption_ctxt(key, iv, 0)
+                self.fail('Expected ValueError to be raised for key %s' % key)
+            except ValueError as err:
+                self.assertTrue(err.message.startswith('Invalid key'))
+
+    def test_invalid_iv(self):
+        key = 'objL7wjV6L79Sfs4y7dy41273l0k6Wki'
+        iv = 'badIv'
+        try:
+            self.crypto.create_encryption_ctxt(key, iv)
+            self.fail('Expected ValueError to be raised')
+        except ValueError as err:
+            self.assertTrue(err.message.startswith('Invalid nonce'))
+        try:
+            self.crypto.create_decryption_ctxt(key, iv, 0)
+            self.fail('Expected ValueError to be raised')
+        except ValueError as err:
+            self.assertTrue(err.message.startswith('Invalid nonce'))
+
+    def test_create_iv(self):
+        self.assertEqual(16, len(self.crypto.create_iv()))
+
+if __name__ == '__main__':
+    unittest.main()
