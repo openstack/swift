faff98efd47fd59e28b1adb3e7ad4bc3dc6dd781
janonymous <janonymous.codevulture@gmail.com>
2015-07-09 07:35:47 +0530

    Replace python print operator with print function (pep H233, py33)
    'print' function is compatible with 2.x and 3.x python versions
    Link : https://www.python.org/dev/peps/pep-3105/
    
    Python 2.6 has a __future__ import that removes print as language syntax,
    letting you use the functional form instead
    
    Change-Id: I9d9ca5766a2773a7b4071e280f00361d16b85383

--- swift/cli/form_signature.py
+++ swift/cli/form_signature.py
@@ -15,6 +15,7 @@
 """
 Script for generating a form signature for use with FormPost middleware.
 """
+from __future__ import print_function
 import hmac
 from hashlib import sha1
 from os.path import basename
@@ -24,41 +25,41 @@ from time import time
 def main(argv):
     if len(argv) != 7:
         prog = basename(argv[0])
-        print 'Syntax: %s <path> <redirect> <max_file_size> ' \
-              '<max_file_count> <seconds> <key>' % prog
-        print
-        print 'Where:'
-        print '  <path>            The prefix to use for form uploaded'
-        print '                    objects. For example:'
-        print '                    /v1/account/container/object_prefix_ would'
-        print '                    ensure all form uploads have that path'
-        print '                    prepended to the browser-given file name.'
-        print '  <redirect>        The URL to redirect the browser to after'
-        print '                    the uploads have completed.'
-        print '  <max_file_size>   The maximum file size per file uploaded.'
-        print '  <max_file_count>  The maximum number of uploaded files'
-        print '                    allowed.'
-        print '  <seconds>         The number of seconds from now to allow'
-        print '                    the form post to begin.'
-        print '  <key>             The X-Account-Meta-Temp-URL-Key for the'
-        print '                    account.'
-        print
-        print 'Example output:'
-        print '    Expires: 1323842228'
-        print '  Signature: 18de97e47345a82c4dbfb3b06a640dbb'
-        print
-        print 'Sample form:'
-        print
+        print('Syntax: %s <path> <redirect> <max_file_size> '
+              '<max_file_count> <seconds> <key>' % prog)
+        print()
+        print('Where:')
+        print('  <path>            The prefix to use for form uploaded')
+        print('                    objects. For example:')
+        print('                    /v1/account/container/object_prefix_ would')
+        print('                    ensure all form uploads have that path')
+        print('                    prepended to the browser-given file name.')
+        print('  <redirect>        The URL to redirect the browser to after')
+        print('                    the uploads have completed.')
+        print('  <max_file_size>   The maximum file size per file uploaded.')
+        print('  <max_file_count>  The maximum number of uploaded files')
+        print('                    allowed.')
+        print('  <seconds>         The number of seconds from now to allow')
+        print('                    the form post to begin.')
+        print('  <key>             The X-Account-Meta-Temp-URL-Key for the')
+        print('                    account.')
+        print()
+        print('Example output:')
+        print('    Expires: 1323842228')
+        print('  Signature: 18de97e47345a82c4dbfb3b06a640dbb')
+        print()
+        print('Sample form:')
+        print()
         print('NOTE: the <form> tag\'s "action" attribute does not contain '
               'the Swift cluster\'s hostname.')
-        print 'You should manually add it before using the form.'
-        print
+        print('You should manually add it before using the form.')
+        print()
         print('<form action="/v1/a/c/o" method="POST" '
               'enctype="multipart/form-data">')
-        print '  <input type="hidden" name="max_file_size" value="123" />'
-        print '  ... more HTML ...'
-        print '  <input type="submit" />'
-        print '</form>'
+        print('  <input type="hidden" name="max_file_size" value="123" />')
+        print('  ... more HTML ...')
+        print('  <input type="submit" />')
+        print('</form>')
         return 1
     path, redirect, max_file_size, max_file_count, seconds, key = argv[1:]
     try:
@@ -66,37 +67,37 @@ def main(argv):
     except ValueError:
         max_file_size = -1
     if max_file_size < 0:
-        print 'Please use a <max_file_size> value greater than or equal to 0.'
+        print('Please use a <max_file_size> value greater than or equal to 0.')
         return 1
     try:
         max_file_count = int(max_file_count)
     except ValueError:
         max_file_count = 0
     if max_file_count < 1:
-        print 'Please use a positive <max_file_count> value.'
+        print('Please use a positive <max_file_count> value.')
         return 1
     try:
         expires = int(time() + int(seconds))
     except ValueError:
         expires = 0
     if expires < 1:
-        print 'Please use a positive <seconds> value.'
+        print('Please use a positive <seconds> value.')
         return 1
     parts = path.split('/', 4)
     # Must be four parts, ['', 'v1', 'a', 'c'], must be a v1 request, have
     # account and container values, and optionally have an object prefix.
     if len(parts) < 4 or parts[0] or parts[1] != 'v1' or not parts[2] or \
             not parts[3]:
-        print '<path> must point to a container at least.'
-        print 'For example: /v1/account/container'
-        print '         Or: /v1/account/container/object_prefix'
+        print('<path> must point to a container at least.')
+        print('For example: /v1/account/container')
+        print('         Or: /v1/account/container/object_prefix')
         return 1
     sig = hmac.new(key, '%s\n%s\n%s\n%s\n%s' % (path, redirect, max_file_size,
                                                 max_file_count, expires),
                    sha1).hexdigest()
-    print '  Expires:', expires
-    print 'Signature:', sig
-    print ''
+    print('  Expires:', expires)
+    print('Signature:', sig)
+    print('')
 
     print('Sample form:\n')
 
