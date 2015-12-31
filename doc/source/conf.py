6786cdf036b4faabe3928c3d0dd9615d94834801
Harshada Mangesh Kakad <harshadak@metsi.co.uk>
2015-12-31 01:44:00 -0800

    Fixing the deprecated library function.
    os.popen() is deprecated since version 2.6. Resolved with use of
    subprocess module.
    
    Change-Id: I4409cdd9edbc1a26d6f99c125c9100fadda5d758
    Partial-Bug: #1529836

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,9 +13,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
 import datetime
+import os
+import subprocess
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -144,8 +145,10 @@ modindex_common_prefix = ['swift.']
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
-git_cmd = "git log --pretty=format:'%ad, commit %h' --date=local -n1"
-html_last_updated_fmt = os.popen(git_cmd).read()
+git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local",
+    "-n1"]
+html_last_updated_fmt = subprocess.Popen(
+    git_cmd, stdout=subprocess.PIPE).communicate()[0]
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
