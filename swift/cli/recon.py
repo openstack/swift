aa7204d106ae33eba9219514f31f008510c9db53
Kazuhiro MIYAHARA <miyahara.kazuhiro@lab.ntt.co.jp>
2016-02-12 20:48:34 +0900

    Remove '#! /usr/bin/env python' from unexecutable files
    'cli/recon.py' and 'cli/ring_builder_analyzer.py' have '#! /usr/bin/env
    python' in spite of they don't have execute permissions.  This patch
    removes '#! /usr/bin/env python' from them.
    
    Change-Id: I1917ccc84b1673af3d862be1796f54595f94c5ca

--- swift/cli/recon.py
+++ swift/cli/recon.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -1084,7 +1083,3 @@ def main():
         reconnoiter.main()
     except KeyboardInterrupt:
         print('\n')
-
-
-if __name__ == '__main__':
-    main()
