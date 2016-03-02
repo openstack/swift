bdfc7070e49a003eb89744df7570f09d65e7438e
Michael Barton <mike@weirdlooking.com>
2016-03-02 20:21:03 +0000

    go: increase default ms_per_part
    25 was apparently too aggressive.
    
    Change-Id: Ic6c061e0b8834d70f7b485e399c1777e60bc5574

--- go/objectserver/replicator.go
+++ go/objectserver/replicator.go
@@ -650,7 +650,7 @@ func NewReplicator(conf string, flags *flag.FlagSet) (hummingbird.Daemon, error)
 	if serverconf.GetBool("object-replicator", "vm_test_mode", false) {
 		replicator.timePerPart = time.Duration(serverconf.GetInt("object-replicator", "ms_per_part", 2000)) * time.Millisecond
 	} else {
-		replicator.timePerPart = time.Duration(serverconf.GetInt("object-replicator", "ms_per_part", 25)) * time.Millisecond
+		replicator.timePerPart = time.Duration(serverconf.GetInt("object-replicator", "ms_per_part", 750)) * time.Millisecond
 	}
 	replicator.concurrency = int(serverconf.GetInt("object-replicator", "concurrency", 1))
 	if replicator.Ring, err = hummingbird.GetRing("object", hashPathPrefix, hashPathSuffix); err != nil {
