0cc9a66cc7e960a5d08ad9caeb1106f66484c0f1
Alistair Coles <alistair.coles@hpe.com>
2016-05-18 15:58:25 +0100

    crypto - test varying proxy pipeline configs
    Adds unit tests to verify behaviour when crypto
    middleware is added/removed from proxy server pipeline.
    
    Re-uses test infrastructure from unit/proxy/test_server.py
    by moving it to unit/helpers.py
    
    Change-Id: I8c83ffad57f813274431b5234e37e5ee825f621d

--- test/unit/common/middleware/test_encrypter_decrypter.py
+++ test/unit/common/middleware/test_encrypter_decrypter.py
@@ -16,14 +16,230 @@ import base64
 import json
 import unittest
 import urllib
+import uuid
 
-from swift.common.middleware import encrypter, decrypter
+from swift.common.middleware import encrypter, decrypter, keymaster
 from swift.common.swob import Request, HTTPCreated, HTTPAccepted
 from swift.common.crypto_utils import CRYPTO_KEY_CALLBACK
 
 from test.unit.common.middleware.crypto_helpers import fetch_crypto_keys, \
     md5hex, encrypt
 from test.unit.common.middleware.helpers import FakeSwift
+from test.unit.helpers import setup_servers, teardown_servers
+
+
+class TestCryptoPipelineChanges(unittest.TestCase):
+    # Tests the consequences of crypto middleware being in/out of the pipeline
+    # for PUT/GET requests on same object. Uses real backend servers so that
+    # the handling of headers and sysmeta is verified to diskfile and back.
+    _test_context = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls._test_context = setup_servers()
+        cls.proxy_app = cls._test_context["test_servers"][0]
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls._test_context is not None:
+            teardown_servers(cls._test_context)
+            cls._test_context = None
+
+    def setUp(self):
+        self.container_path = 'http://localhost:8080/v1/a/' + uuid.uuid4().hex
+        self.object_path = self.container_path + '/o'
+        self.plaintext = 'unencrypted body content'
+        self.plaintext_etag = md5hex(self.plaintext)
+
+        # Set up a pipeline of crypto middleware ending in the proxy app so
+        # that tests can make requests to either the proxy server directly or
+        # via the crypto middleware. Make a fresh instance for each test to
+        # avoid any state coupling.
+        enc = encrypter.Encrypter(self.proxy_app, {})
+        km = keymaster.KeyMaster(enc, {'encryption_root_secret': 's3cr3t'})
+        self.crypto_app = decrypter.Decrypter(km, {})
+
+    def _create_container(self, app, policy_name='one'):
+        req = Request.blank(
+            self.container_path, method='PUT',
+            headers={'X-Storage-Policy': policy_name})
+        resp = req.get_response(app)
+        self.assertEqual('201 Created', resp.status)
+        # sanity check
+        req = Request.blank(
+            self.container_path, method='HEAD',
+            headers={'X-Storage-Policy': policy_name})
+        resp = req.get_response(app)
+        self.assertEqual(policy_name, resp.headers['X-Storage-Policy'])
+
+    def _put_object(self, app, body):
+        req = Request.blank(self.object_path, method='PUT', body=body)
+        resp = req.get_response(app)
+        self.assertEqual('201 Created', resp.status)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+        return resp
+
+    def _post_object(self, app):
+        req = Request.blank(self.object_path, method='POST',
+                            headers={'X-Object-Meta-Fruit': 'Kiwi'})
+        resp = req.get_response(app)
+        self.assertEqual('202 Accepted', resp.status)
+        return resp
+
+    def _check_GET_and_HEAD(self, app):
+        req = Request.blank(self.object_path, method='GET')
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertEqual(self.plaintext, resp.body)
+        self.assertEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+        req = Request.blank(self.object_path, method='HEAD')
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertEqual('', resp.body)
+        self.assertEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+    def _check_match_requests(self, method, app):
+        # verify conditional match requests
+        expected_body = self.plaintext if method == 'GET' else ''
+
+        # If-Match matches
+        req = Request.blank(self.object_path, method=method,
+                            headers={'If-Match': '"%s"' % self.plaintext_etag})
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertEqual(expected_body, resp.body)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+        self.assertEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+        # If-Match wildcard
+        req = Request.blank(self.object_path, method=method,
+                            headers={'If-Match': '*'})
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertEqual(expected_body, resp.body)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+        self.assertEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+        # If-Match does not match
+        req = Request.blank(self.object_path, method=method,
+                            headers={'If-Match': '"not the etag"'})
+        resp = req.get_response(app)
+        self.assertEqual('412 Precondition Failed', resp.status)
+        self.assertEqual('', resp.body)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+
+        # If-None-Match matches
+        req = Request.blank(
+            self.object_path, method=method,
+            headers={'If-None-Match': '"%s"' % self.plaintext_etag})
+        resp = req.get_response(app)
+        self.assertEqual('304 Not Modified', resp.status)
+        self.assertEqual('', resp.body)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+
+        # If-None-Match wildcard
+        req = Request.blank(self.object_path, method=method,
+                            headers={'If-None-Match': '*'})
+        resp = req.get_response(app)
+        self.assertEqual('304 Not Modified', resp.status)
+        self.assertEqual('', resp.body)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+
+        # If-None-Match does not match
+        req = Request.blank(self.object_path, method=method,
+                            headers={'If-None-Match': '"not the etag"'})
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertEqual(expected_body, resp.body)
+        self.assertEqual(self.plaintext_etag, resp.headers['Etag'])
+        self.assertEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+    def _check_listing(self, app, expect_mismatch=False):
+        req = Request.blank(
+            self.container_path, method='GET', query_string='format=json')
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        listing = json.loads(resp.body)
+        self.assertEqual(1, len(listing))
+        self.assertEqual('o', listing[0]['name'])
+        self.assertEqual(len(self.plaintext), listing[0]['bytes'])
+        if expect_mismatch:
+            self.assertNotEqual(self.plaintext_etag, listing[0]['hash'])
+        else:
+            self.assertEqual(self.plaintext_etag, listing[0]['hash'])
+
+    def test_write_with_crypto_read_with_crypto(self):
+        self._create_container(self.proxy_app, policy_name='one')
+        self._put_object(self.crypto_app, self.plaintext)
+        self._post_object(self.crypto_app)
+        self._check_GET_and_HEAD(self.crypto_app)
+        self._check_match_requests('GET', self.crypto_app)
+        self._check_match_requests('HEAD', self.crypto_app)
+        self._check_listing(self.crypto_app)
+
+    def test_write_with_crypto_read_with_crypto_ec(self):
+        self._create_container(self.proxy_app, policy_name='ec')
+        self._put_object(self.crypto_app, self.plaintext)
+        self._post_object(self.crypto_app)
+        self._check_GET_and_HEAD(self.crypto_app)
+        self._check_match_requests('GET', self.crypto_app)
+        self._check_match_requests('HEAD', self.crypto_app)
+        self._check_listing(self.crypto_app)
+
+    def test_write_without_crypto_read_with_crypto(self):
+        self._create_container(self.proxy_app, policy_name='one')
+        self._put_object(self.proxy_app, self.plaintext)
+        self._post_object(self.proxy_app)
+        self._check_GET_and_HEAD(self.proxy_app)  # sanity check
+        self._check_GET_and_HEAD(self.crypto_app)
+        self._check_match_requests('GET', self.proxy_app)  # sanity check
+        self._check_match_requests('GET', self.crypto_app)
+        self._check_match_requests('HEAD', self.proxy_app)  # sanity check
+        self._check_match_requests('HEAD', self.crypto_app)
+        self._check_listing(self.crypto_app)
+
+    def _check_GET_and_HEAD_not_decrypted(self, app):
+        req = Request.blank(self.object_path, method='GET')
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertNotEqual(self.plaintext, resp.body)
+        self.assertEqual('%s' % len(self.plaintext),
+                         resp.headers['Content-Length'])
+        self.assertNotEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+        req = Request.blank(self.object_path, method='HEAD')
+        resp = req.get_response(app)
+        self.assertEqual('200 OK', resp.status)
+        self.assertEqual('', resp.body)
+        self.assertNotEqual('Kiwi', resp.headers['X-Object-Meta-Fruit'])
+
+    def test_write_with_crypto_read_without_crypto(self):
+        self._create_container(self.proxy_app, policy_name='one')
+        self._put_object(self.crypto_app, self.plaintext)
+        self._post_object(self.crypto_app)
+        self._check_GET_and_HEAD(self.crypto_app)  # sanity check
+        # without crypto middleware, GET and HEAD returns ciphertext
+        self._check_GET_and_HEAD_not_decrypted(self.proxy_app)
+        self._check_listing(self.proxy_app, expect_mismatch=True)
+
+    def test_write_with_crypto_read_without_crypto_ec(self):
+        self._create_container(self.proxy_app, policy_name='ec')
+        self._put_object(self.crypto_app, self.plaintext)
+        self._post_object(self.crypto_app)
+        self._check_GET_and_HEAD(self.crypto_app)  # sanity check
+        # without crypto middleware, GET and HEAD returns ciphertext
+        self._check_GET_and_HEAD_not_decrypted(self.proxy_app)
+        self._check_listing(self.proxy_app, expect_mismatch=True)
+
+
+class TestCryptoPipelineChangesFastPost(TestCryptoPipelineChanges):
+    @classmethod
+    def setUpClass(cls):
+        # set proxy config to use fast post
+        extra_conf = {'object_post_as_copy': 'False'}
+        cls._test_context = setup_servers(extra_conf=extra_conf)
+        cls.proxy_app = cls._test_context["test_servers"][0]
 
 
 class TestEncrypterDecrypter(unittest.TestCase):
