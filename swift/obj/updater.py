ede42c77f1e04ab571c21b1d6342e662c7eced53
Takashi Kajinami <kajinamit@nttdata.co.jp>
2016-09-30 02:08:25 +0900

    Ensure update of the container by object-updater
    Object-updater regards 404 from container-server as success.
    This causes wrong giving up and generates object which never appears
    at container, for example when all container replicas are placed in
    handoff devices at the moment.
    
    This patch makes object-updater try updating until it gets 201s from
    all container servers, and ensures the update.
    
    Change-Id: Ic5d55ff368cec62ab2eb9fd883801ba4ab043dd4
    Closes-Bug: #1328735

--- swift/obj/updater.py
+++ swift/obj/updater.py
@@ -31,8 +31,7 @@ from swift.common.utils import get_logger, renamer, write_pickle, \
 from swift.common.daemon import Daemon
 from swift.common.storage_policy import split_policy_string, PolicyError
 from swift.obj.diskfile import get_tmp_dir, ASYNCDIR_BASE
-from swift.common.http import is_success, HTTP_NOT_FOUND, \
-    HTTP_INTERNAL_SERVER_ERROR
+from swift.common.http import is_success, HTTP_INTERNAL_SERVER_ERROR
 
 
 class ObjectUpdater(Daemon):
@@ -269,8 +268,13 @@ class ObjectUpdater(Daemon):
             with Timeout(self.node_timeout):
                 resp = conn.getresponse()
                 resp.read()
-                success = (is_success(resp.status) or
-                           resp.status == HTTP_NOT_FOUND)
+                success = is_success(resp.status)
+                if not success:
+                    self.logger.error(
+                        _('Error code %(status)d is returned from remote '
+                          'server %(ip)s: %(port)s / %(device)s'),
+                        {'status': resp.status, 'ip': node['ip'],
+                         'port': node['port'], 'device': node['device']})
                 return (success, node['id'])
         except (Exception, Timeout):
             self.logger.exception(_('ERROR with remote server '
--- test/unit/obj/test_updater.py
+++ test/unit/obj/test_updater.py
@@ -362,7 +362,7 @@ class TestObjectUpdater(unittest.TestCase):
         self.assertEqual([0],
                          pickle.load(open(op_path)).get('successes'))
 
-        event = spawn(accept, [404, 500])
+        event = spawn(accept, [404, 201])
         cu.logger._clear()
         cu.run_once()
         err = event.wait()
@@ -371,7 +371,7 @@ class TestObjectUpdater(unittest.TestCase):
         self.assertTrue(os.path.exists(op_path))
         self.assertEqual(cu.logger.get_increment_counts(),
                          {'failures': 1})
-        self.assertEqual([0, 1],
+        self.assertEqual([0, 2],
                          pickle.load(open(op_path)).get('successes'))
 
         event = spawn(accept, [201])
