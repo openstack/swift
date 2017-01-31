90d517f10ef0c2c651f8ffe92c7da3dcfe2cd83a
Samuel Merritt <sam@swiftstack.com>
2017-01-30 16:05:23 -0800

    Remove some cruft in test_proxy_logging
    Change-Id: I7533d671a62d456d4556435aad78aab389c6cf07

--- test/unit/common/middleware/test_proxy_logging.py
+++ test/unit/common/middleware/test_proxy_logging.py
@@ -102,10 +102,6 @@ def start_response(*args):
 
 @patch_policies([StoragePolicy(0, 'zero', False)])
 class TestProxyLogging(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
     def _log_parts(self, app, should_be_empty=False):
         info_calls = app.access_logger.log_dict['info']
         if should_be_empty:
@@ -1035,7 +1031,3 @@ class TestProxyLogging(unittest.TestCase):
             ''.join(resp)
         log_parts = self._log_parts(app)
         self.assertEqual(log_parts[20], '1')
-
-
-if __name__ == '__main__':
-    unittest.main()
