bdd4eb6936b0e25aff5357bde876309ee5b032ec
Andreas Jaeger <aj@suse.com>
2017-12-20 07:14:03 +0100

    Install liberasurecode-devel for CentOS 7
    Since I747c2b8754effbc6ec82af3bf7543fd9599a6c14 we do not install
    the RDO package repository anymore and thus liberasurecode-devel
    cannot be installed.
    
    For CentOS 7, remove liberasurecode-devel from bindep.txt and install it
    from test-setup.sh instead after enabling the RDO package repositories.
    
    Update python dependencies: CentOS 7 does not have python3. Fix the
    SUSE tags.
    
    Change-Id: I72aa6b5455dfb025f54e83334983ac280f04afb2

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -12,3 +12,19 @@ truncate -s 1GB $HOME/1G_xfs_file
 mkdir -p $HOME/xfstmp
 sudo mount -o loop,noatime,nodiratime $HOME/1G_xfs_file $HOME/xfstmp
 sudo chmod 777 $HOME/xfstmp
+
+# Install liberasurecode-devel for CentOS from RDO repository.
+
+function is_rhel7 {
+    [ -f /usr/bin/yum ] && \
+        cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
+        cat /etc/*release | grep -q 'release 7'
+}
+
+
+if is_rhel7; then
+    # Install CentOS OpenStack repos so that we have access to some extra
+    # packages.
+    sudo yum install -y centos-release-openstack-pike
+    sudo yum install -y liberasurecode-devel
+fi
