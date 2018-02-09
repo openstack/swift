a3d2aaba6470645345b19d52f0dbbfd51a4d5cf4
baiwenteng <baiwenteng@inspur.com>
2018-02-09 12:20:59 +0800

    Fix typos in swift
    Change-Id: I0982b0046a16fda0a39d9b31402b2e4b3160a5c4

--- swift/proxy/controllers/obj.py
+++ swift/proxy/controllers/obj.py
@@ -2263,7 +2263,7 @@ class ECObjectController(BaseObjectController):
         for client_start, client_end in req.range.ranges:
             # TODO: coalesce ranges that overlap segments. For
             # example, "bytes=0-10,20-30,40-50" with a 64 KiB
-            # segment size will result in a a Range header in the
+            # segment size will result in a Range header in the
             # object request of "bytes=0-65535,0-65535,0-65535",
             # which is wasteful. We should be smarter and only
             # request that first segment once.
