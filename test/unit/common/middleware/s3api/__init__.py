58352b285d6532321c6dfbe0e94218799c615ef2
Clay Gerrard <clay.gerrard@gmail.com>
2019-04-29 13:59:39 -0500

    s3api multi upload test clean up
    Add a test assertion for invalid ETag of MPU manifest and make some
    global constants more obvious.
    
    Change-Id: I830c0d2e473d1740f9138ebc6efd64a39d85d1dc
    Related-Change-Id: I7df13e670f702a2956a9c8057fcb971f4bfb7319

--- test/unit/common/middleware/s3api/__init__.py
+++ test/unit/common/middleware/s3api/__init__.py
@@ -25,6 +25,7 @@ from swift.common.middleware.s3api.etree import fromstring
 from swift.common.middleware.s3api.utils import Config
 
 from test.unit.common.middleware.s3api.helpers import FakeSwift
+from test.unit import debug_logger
 
 
 class FakeApp(object):
@@ -79,6 +80,7 @@ class S3ApiTestCase(unittest.TestCase):
         self.app = FakeApp()
         self.swift = self.app.swift
         self.s3api = filter_factory({}, **self.conf)(self.app)
+        self.s3api.logger = debug_logger()
 
         self.swift.register('HEAD', '/v1/AUTH_test',
                             swob.HTTPOk, {}, None)
