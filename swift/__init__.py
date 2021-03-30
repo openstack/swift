0d44328c7500d688f7b398181ce7e6625d6fd178
Tim Burke <tim.burke@gmail.com>
2021-03-30 14:02:36 -0700

    Quiet more deprecation warnings on py2
    Change-Id: I309767b2c6352e097d9572053c08d8377806809c

--- swift/__init__.py
+++ swift/__init__.py
@@ -81,3 +81,7 @@ warnings.filterwarnings('ignore', module='cryptography|OpenSSL', message=(
     'Python 2 is no longer supported by the Python core team. '
     'Support for it is now deprecated in cryptography, '
     'and will be removed in a future release.'))
+warnings.filterwarnings('ignore', module='cryptography|OpenSSL', message=(
+    'Python 2 is no longer supported by the Python core team. '
+    'Support for it is now deprecated in cryptography, '
+    'and will be removed in the next release.'))
