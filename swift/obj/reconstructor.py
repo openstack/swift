2696a79f098b02988136b13caf1c2565ec09481f
Alistair Coles <alistairncoles@gmail.com>
2021-07-16 12:27:25 +0100

    reconstructor: retire nondurable_purge_delay option
    The nondurable_purge_delay option was introduced in [1] to prevent the
    reconstructor removing non-durable data files on handoffs that were
    about to be made durable. The DiskFileManager commit_window option has
    since been introduced [2] which specifies a similar time window during
    which non-durable data files should not be removed. The commit_window
    option can be re-used by the reconstructor, making the
    nondurable_purge_delay option redundant.
    
    The nondurable_purge_delay option has not been available in any tagged
    release and is therefore removed with no backwards compatibility.
    
    [1] Related-Change: I0d519ebaaade35249fb7b17bd5f419ffdaa616c0
    [2] Related-Change: I5f3318a44af64b77a63713e6ff8d0fd3b6144f13
    Change-Id: I1589a7517b7375fcc21472e2d514f26986bf5079

--- swift/obj/reconstructor.py
+++ swift/obj/reconstructor.py
@@ -34,7 +34,7 @@ from swift.common.utils import (
     GreenAsyncPile, Timestamp, remove_file,
     load_recon_cache, parse_override_options, distribute_evenly,
     PrefixLoggerAdapter, remove_directory, config_request_node_count_value,
-    non_negative_int, non_negative_float)
+    non_negative_int)
 from swift.common.header_key_dict import HeaderKeyDict
 from swift.common.bufferedhttp import http_connect
 from swift.common.daemon import Daemon
@@ -241,8 +241,6 @@ class ObjectReconstructor(Daemon):
                      conf.get('reclaim_age', DEFAULT_RECLAIM_AGE)))
         self.request_node_count = config_request_node_count_value(
             conf.get('request_node_count', '2 * replicas'))
-        self.nondurable_purge_delay = non_negative_float(
-            conf.get('nondurable_purge_delay', '60'))
 
         # When upgrading from liberasurecode<=1.5.0, you may want to continue
         # writing legacy CRCs until all nodes are upgraded and capabale of
@@ -986,7 +984,7 @@ class ObjectReconstructor(Daemon):
                 # know the data file is durable so that legacy durable data
                 # files get purged
                 nondurable_purge_delay = (0 if timestamps.get('durable')
-                                          else self.nondurable_purge_delay)
+                                          else df_mgr.commit_window)
                 df.purge(timestamps['ts_data'], frag_index,
                          nondurable_purge_delay)
             except DiskFileError:
