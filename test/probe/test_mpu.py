fa209bd1b18ead0ea8cc0670aeac458d97b93537
Alistair Coles <alistairncoles@gmail.com>
2024-10-02 12:58:28 +0100

    mpu: complete mpu with user content-type when available
    If the user sent a content-type header with a createUpload request
    then, during completeUpload, MPU middleware sends that with the
    manifest PUT and the symlink PUT.
    
    If the user did not send a content-type header with a createUpload
    request then, during completeUpload, MPU middleware sends a default
    'application/x-mpu' with the manifest PUT and sends no content-type
    with the symlink PUT. The proxy app will guess content-type for the
    symlink based on the user namespace path.
    
    Also clarify some module constant names.
    
    Change-Id: Ib3083a7e987c0e6ed921c68b44f074aebd4af5c3

--- test/probe/test_mpu.py
+++ test/probe/test_mpu.py
@@ -474,7 +474,7 @@ class TestNativeMPU(BaseTestMPU):
             self.url, self.token, self.bucket_name, query_string='uploads')
         self.assertEqual(['%s/%s' % (self.mpu_name, upload_id)],
                          [o['name'] for o in listing])
-        self.assertEqual(['application/x-mpu'],
+        self.assertEqual(['application/x-mpu-session'],
                          [o['content_type'] for o in listing])
         # upload part
         part_size = 5 * 2 ** 20
