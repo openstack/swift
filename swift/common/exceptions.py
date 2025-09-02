99c7a8b2c5ef104f0b17c736a9915e918fb10589
Christian Schwede <cschwede@redhat.com>
2025-09-02 16:24:38 +0200

    WIP: Add dummy Timeout exception
    Timeout() is both used as an Exception class as well as a context
    manager to limit the execution time. Especially the latter needs to be
    either reimplemented or changed to a different approach; this is just a
    dummy to make the POC work.
    
    Change-Id: I1dee12eb9f85513b1ea29de6ae6451e3b7a5f92e
    Signed-off-by: Christian Schwede <cschwede@redhat.com>

--- swift/common/exceptions.py
+++ swift/common/exceptions.py
@@ -13,8 +13,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from eventlet import Timeout
 import swift.common.utils
+
+from swift.common.utils import eventlet_disabled
+if not eventlet_disabled():
+    from eventlet import Timeout
+else:
+    class Timeout(BaseException):
+        """Dummy eventlet.Timeout-like that should never get raised"""
+
+        def __init__(self, seconds=None, exception=None):
+            self.start()
+
+        def start(self):
+            return self
+
+        @property
+        def pending(self):
+            return False
+
+        def cancel(self):
+            pass
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, typ, value, tb):
+            return True
+
+        @property
+        def is_timeout(self):
+            return True
 
 
 class MessageTimeout(Timeout):
